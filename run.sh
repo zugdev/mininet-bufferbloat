bef6a44297652761d581370b7bc01125bb3e430c
zugdev <zug.dev@proton.me>
2024-11-25 15:58:26 -0300

    chore: move

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ time=90
 bwnet=1.5
 # TODO: If you want the RTT to be 20ms what should the delay on each
 # link be?  Set this value correctly.
-delay=
+delay=10ms
 
 iperf_port=5001
 
@@ -15,7 +15,7 @@ for qsize in 20 100; do
     dir=bb-q$qsize
 
     # TODO: Run bufferbloat.py here...
-    # python3 ...
+    python3 bufferbloat.py --bw $bwnet --delay $delay --time $time --maxq $qsize --dir $dir
 
     # TODO: Ensure the input file names match the ones you use in
     # bufferbloat.py script.  Also ensure the plot file names match
